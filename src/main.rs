6515ece6a1d312dc0ba4445cf7e82c1acc9d8b50
Bourgond Aries <macocio@gmail.com>
2016-05-27 04:14:12 +0200

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(feature="clippy", allow(items_after_statements))]
+
+extern crate acon;
+
 fn main() {
 }
 
